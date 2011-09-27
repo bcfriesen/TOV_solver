f80a57accabca458a0fe632945adec9a8e8822c2
Brian Friesen <friesen@ou.edu>
2011-09-27 14:00:36 +0200

    looping over different values of P(r=0) and outputting total mass M(r=R).
    as suggested in the GSL manual, I set the ODE function
    return value to GSL_EBADFUNC if either 1.) the pressure at a given
    radius is negative, or 2.) if either derivative yields a NaN (which
    usually happens at the same radius where P < 0). this return value
    causes the integrator to stop. I then dump the last result for
    M(r), which should be M(r=R). However that seems not always to
    work. In particular, if you printf M(r) and r in the ODE function
    itself, as well as printf from the main program after the ODE
    function has returned GSL_EBADFUNC, you'll find they don't match;
    in particular, the integrator seems to progress several steps
    farther in r than indicated by the "last value" printf. no idea
    why this is.
    
    however, even with this weird "feature," a plot of P(r = 0) and
    M(r = R) looks good: there's definitely a global maximum M(R) which
    hopefully will be the Chandra mass when I put the units back. however
    there are a few huge spikes and dips which probably correspond to
    that weird thing I just talked about.

--- main.c
+++ main.c
@@ -8,27 +8,82 @@
 
 int func (double t, const double y[], double f[], void *params)
 {
+  if ((gsl_isnan(f[0]) || gsl_isnan(f[1])) || y[1] < 0.0) {return GSL_EBADFUNC;}
+    
   double Gamma = *(double *)params;
-  printf("r = %12f; y[0] = %12f; y[1] = %12f\n", t, y[0], y[1]);
+//  printf("r = %12f; M[0] = %12f; P[1] = %12f\n", t, y[0], y[1]);
   f[0] = 4.0*M_PI*pow(t, 2.0)*pow(y[1], 1.0/Gamma);
   f[1] = -(pow(y[1], 1.0/Gamma)*y[0]/pow(t, 2.0))*(1.0 + pow(y[1], (Gamma - 1.0)/Gamma))*(1.0 + (4.0*M_PI*y[1]*pow(t, 3.0))/y[0])*
          pow(1.0 - 2.0*y[0]/t, -1.0);
- // printf("f[0] = %12f; f[1] = %12f\n", f[0], f[1]);
+
   return GSL_SUCCESS;
 }
+
 
 int main (void)
 {
   double Gamma = 5.0/3.0;
   int *fake_jac = NULL;
-  double t = 1.0e-5, t1 = 2.0;
-  double y[2] = { 1.0e-6, 1.0 };
+  double t, t1;
+  double y[2], pinit;
+  const double tiny = 1.0e-5;
+  const double pmin = tiny, pmax = 0.3;
+  int i;
+  const int MAX = 1000;
+  int status;
 
-  gsl_odeiv2_system sys = {func, fake_jac, 2, &Gamma};
+/*  gsl_odeiv2_system sys = {func, fake_jac, 2, &Gamma};
 
   gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd, 1.0e-6, 1.0e-6, 0.0);
 
-  int status = gsl_odeiv2_driver_apply(d, &t, t1, y);
+  for (i = 0; i < MAX; i++)
+  {
+    t = 1.0e-5;
+    t1 = 3.0;
+    y[0] = 1.0e-6; // mass always starts at 0
+    y[1] = 1.0e-2 + (double)i;
+    printf("y[0] = %12f; y[1] = %12f\n", y[0], y[1]);
+    int status = gsl_odeiv2_driver_apply(d, &t, t1, y);
+  }
+
+  t = 1.0e-5;
+  t1 = 3.0;
+  y[0] = 1.0e-6;
+  y[1] = 1.0;
+  status = gsl_odeiv2_driver_apply(d, &t, t1, y);
+
+  gsl_odeiv2_driver_free (d);
+
+  t = 1.0e-5;
+  t1 = 3.0;
+  y[0] = 1.0e-6;
+  y[1] = 1.0; */
+
+  printf("%12s %12s %12s\n", "RADIUS", "M(r = R)", "P(r = 0)");
+  for (i = 0; i < MAX; i++)
+  {
+    t = 1.0e-5;
+    t1 = 3.0;
+    y[0] = 1.0e-6; // mass always starts at 0
+    y[1] = pmin + (double)i*((pmax - pmin)/(double)MAX);
+    pinit = y[1];
+//    printf("M[0] = %12f; P[1] = %12f\n", y[0], y[1]);
+    status = make_grid(Gamma, &t, t1, y);
+    printf("%12f %12f %12f\n", t, y[0], pinit);
+  }
+  
+  return 0;
+}
+
+
+int make_grid (double Gamma, double *t, double t1, double y[])
+{
+  int status;
+  int *fake_jac = NULL;
+  gsl_odeiv2_system sys = {func, fake_jac, 2, &Gamma};
+  gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd, 1.0e-6, 1.0e-6, 0.0);
+
+  status = gsl_odeiv2_driver_apply(d, t, t1, y);
 
   gsl_odeiv2_driver_free (d);
   return 0;
