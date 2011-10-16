a5d5aadcf9b8cae2a0b7722783236f640dafbc1c
Brian Friesen <friesen@ou.edu>
2011-10-16 18:12:18 +0200

    more comments since I'll forget why I did this crap

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <gsl/gsl_math.h>
 #include <math.h>
 
-struct param
+struct param // parameter list to be passed to ODEs
 {
   double Gamma, pinit;
 };
@@ -15,16 +15,22 @@ int func (double t, const double y[], double f[], void *params)
 {
   struct param myparams = *(struct param *)params;
 
-  if (y[1] < 0.0)
+  if (y[1] < 0.0) // can't have negative pressure
   {
     printf("%12f %12f %12f\n", t, y[0], myparams.pinit);
-    return GSL_EBADFUNC; // can't have negative pressure
+    return GSL_EBADFUNC; // this flag tells GSL integrator to quit
   } 
 
+  // ODEs written explicit in terms of polytropic EOS. this should change
+  // because it's not flexible.
   f[0] = 4.0*M_PI*pow(t, 2.0)*pow(y[1], 1.0/myparams.Gamma);
-  f[1] = -(pow(y[1], 1.0/myparams.Gamma)*y[0]/pow(t, 2.0))*(1.0 + pow(y[1], (myparams.Gamma - 1.0)/myparams.Gamma))*(1.0 + (4.0*M_PI*y[1]*pow(t, 3.0))/y[0])*
-         pow(1.0 - 2.0*y[0]/t, -1.0);
-  if ((gsl_isnan(f[0]) || gsl_isnan(f[1])))
+  f[1] = -(pow(y[1], 1.0/myparams.Gamma)*y[0]/pow(t, 2.0))*(1.0 +
+           pow(y[1], (myparams.Gamma - 1.0)/myparams.Gamma))*(1.0 +
+           (4.0*M_PI*y[1]*pow(t, 3.0))/y[0])*
+           pow(1.0 - 2.0*y[0]/t, -1.0);
+
+// ODEs tend to return NaNs at the same radius where P < 0
+  if ((gsl_isnan(f[0]) || gsl_isnan(f[1]))) 
   {
     printf("%12f %12f %12f\n", t, y[0], myparams.pinit);
     return GSL_EBADFUNC;
@@ -38,7 +44,6 @@ int main (void)
 {
   struct param params;
   params.Gamma = 5.0/3.0;
-  int *fake_jac = NULL;
   double t, t1;
   double y[2];
   const double tiny = 1.0e-5;
@@ -51,7 +56,7 @@ int main (void)
   {
     t = 1.0e-5;
     t1 = 3.0;
-    y[0] = 1.0e-6; // mass always starts at 0
+    y[0] = 1.0e-6; // central mass always starts at 0
     y[1] = pmin + (double)i*((pmax - pmin)/(double)MAX);
     params.pinit = y[1];
     status = make_grid(params, &t, t1, y);
@@ -60,13 +65,13 @@ int main (void)
   return 0;
 }
 
-
 int make_grid (struct param params, double *t, double t1, double y[])
 {
   int status;
-  int *fake_jac = NULL;
+  int *fake_jac = NULL; // the integrator RK8PD doesn't need the jacobian
   gsl_odeiv2_system sys = {func, fake_jac, 2, &params};
-  gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd, 1.0e-6, 1.0e-6, 0.0);
+  gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new
+                         (&sys, gsl_odeiv2_step_rk8pd, 1.0e-6, 1.0e-6, 0.0);
 
   status = gsl_odeiv2_driver_apply(d, t, t1, y);
 
